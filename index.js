f17d3a51fa07c442e80a9242d8915da45e84f90f
Looovelllyy <sedykh.k.n@gmail.com>
2022-03-23 14:45:18 +0300

    update

--- index.js
+++ index.js
@@ -12,31 +12,43 @@ function readFile(input) {
     let counterForAnsw = 1
     reader.readAsText(file);
 
-    reader.onload = function() {
+    let newObj = {} // {quest: string, answ: string[], correct: string}
+    // let reg = new RegExp('[1-9]');
+    let reg = /[1-9]/
+    let regAnsw = /correct/
+
+    reader.onload = function () {
         inputText = reader.result;
-        let testArr = inputText.split('\r\n');
-        for (let i = 0; i < testArr.length; i++) {
-            // console.log(+testArr[i][0])
-            if (!(isNaN(+testArr[i][0]))) {
-                let key = testArr[i][0];
-                let value = testArr[i].split(' ').slice(1).join(' ');
-                objArr.push({id: counterForQues, [key]: value})
-                counterForQues++
-            }
-            if (!(testArr[i].indexOf('correct') === -1)) {
-                let value = testArr[i].split('correct: ').join('');
-                answers.push({id: counterForAnsw, answer: value});
-                counterForAnsw++
-            }
-            if (isNaN(+testArr[i][0])){
+        let testObj = inputText.split(reg)[1];
+        let testAnsw = testObj.split(regAnsw)
+        console.log(testAnsw);
+    }
 
-            }
-        }
-        console.log(objArr);
-        console.log(answers);
-        // }
-
-    };
+    // reader.onload = function() {
+    //     inputText = reader.result;
+    //     let testArr = inputText.split('\r\n');
+    //     for (let i = 0; i < testArr.length; i++) {
+    //         // console.log(+testArr[i][0])
+    //         if (!(isNaN(+testArr[i][0]))) {
+    //             let key = testArr[i][0];
+    //             let value = testArr[i].split(' ').slice(1).join(' ');
+    //             objArr.push({id: counterForQues, [key]: value})
+    //             counterForQues++
+    //         }
+    //         if (!(testArr[i].indexOf('correct') === -1)) {
+    //             let value = testArr[i].split('correct: ').join('');
+    //             answers.push({id: counterForAnsw, answer: value});
+    //             counterForAnsw++
+    //         }
+    //         if (isNaN(+testArr[i][0])){
+    //
+    //         }
+    //     }
+    //     console.log(objArr);
+    //     console.log(answers);
+    //     // }
+    //
+    // };
 
 }
 
